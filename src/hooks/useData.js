a46ffebec9c4ccce944f115f11fb940ac26cfe0e
Abul Syed <syed1234@hotmail.co.uk>
2021-02-18 20:25:16 +0000

    useEffect

---
+++ src/hooks/useData.js
@@ -0,0 +1,32 @@
+import { useState, useEffect } from 'react';
+import yelp from '../api/yelp';
+
+const useData = () => {
+    const [data, setData] = useState([]);
+    const [error, setError] = useState(null);
+
+    const searchApi = async (searchTerm) => {
+        try {
+            const response = await yelp.get('/search', {
+                params: {
+                    term: searchTerm,
+                    location: 'london',
+                    limit: 50
+                }
+            });
+            setData(response.data.businesses);
+            setError(null);
+        }catch(error){
+            setError(error.message);
+            console.log(error.message);
+        }
+    }
+
+    useEffect(() => {
+        searchApi('pasta');
+    }, []);
+
+    return { data, error, searchApi }
+}
+
+export default useData;
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,33 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import SearchBar from '../components/SearchBar';
-import yelp from '../api/yelp';
+import useData from '../hooks/useData';
 
 const SearchScreen = () => {
     const [term, setTerm] = useState('');
-    const [data, setData] = useState([]);
-    const [error, setError] = useState(null);
-
-    const searchApi = async (searchTerm) => {
-        try {
-            const response = await yelp.get('/search', {
-                params: {
-                    term: searchTerm,
-                    location: 'london',
-                    limit: 50
-                }
-            });
-            setData(response.data.businesses);
-            setError(null);
-        }catch(error){
-            setError(error.message);
-            console.log(error.message);
-        }
-    }
-
-    useEffect(() => {
-        searchApi('pasta');
-    }, []);
+    const { data, error, searchApi } = useData();
 
     return ( 
         <View>
